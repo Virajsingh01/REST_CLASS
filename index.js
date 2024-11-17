83864007851bc7cfd4f75424dfd9049933d2c185
Viraj Singh <rajputviraj0007@gmail.com>
2024-11-17 18:22:18 +0530

    hwy

--- index.js
+++ index.js
@@ -61,9 +61,19 @@ res.redirect("/posts");
 
 app.get("/posts/:id/edit" ,(req ,res) =>{
     let {id} =req.params;
-    let post =posts.find((p) => id ===p.id); 
+    posts =posts.find((p) => id !==p.id); 
     res.render("edit.ejs" ,{post});  
-})
+});
+
+app.delete("/posts/:id" ,(req,res) =>{
+    let {id} =req.params;
+    posts =posts.filter((p) => id !==p.id); 
+   //  res.send("delete succes");
+    
+res.redirect("/posts");
+});
 app.listen(port ,()=>{
 console.log("listening to port:8080");
 });
+
+//helooooo
